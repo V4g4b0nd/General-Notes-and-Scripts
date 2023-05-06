b33020eb324ffcf807ed97504e8163c62bf2812c
Poovin <108090982+V4g4b0nd@users.noreply.github.com>
2023-05-06 12:26:28 +0530

    Create wfuzz-Openssl-fix.sh
    Usage :
    
    download the script and change the permission
    chmod +x wfuzz-Openssl-fix.sh
    
    run ./wfuzz-Openssl-fix.sh
    take a Coffee break

---
+++ tool-fixes/wfuzz-Openssl-fix.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Fixing Warning: Pycurl is not compiled against Openssl. Wfuzz might not work correctly when fuzzing SSL sites
+
+#need apt, pip and python
+
+sudo apt update
+sudo apt-get install python3-pip
+
+# remove the whole installed wfuzz and pycurl
+sudo apt --purge remove python3-pycurl  
+sudo apt-get purge wfuzz    
+sudo pip uninstall wfuzz -y
+#install the delow libraries
+
+sudo apt install libcurl4-gnutls-dev librtmp-dev  -y   
+sudo apt install libcurl4-openssl-dev libssl-dev -y 
+
+#install the wfuzz from pip
+
+sudo pip install wfuzz
+
+#run wfuzz in terminal and we won't get the Openssl warning
+
+wfuzz
